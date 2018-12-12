c0204db9b85b498fd93d30ea6bb52251405652bc
lefreire <letfreirefigueiredo@gmail.com>
2018-12-12 16:32:10 -0200

    Adding news views to collect data

--- dbaas/physical/urls.py
+++ dbaas/physical/urls.py
@@ -5,5 +5,7 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     'physical.views',
     url(r'^engines_by_env/(?P<environment_id>\d+)/$', "engines_by_env"),
+    url(r'^topology_by_eng/(?P<engine_id>\d+)/$', "topology_by_eng"),
+    url(r'^offering_by_engine/(?P<engine_id>\d+)_(?P<environment_id>\d+)/$', "offering_by_engine"),
     url(r'^plans_details/$', "plans_details"),
 )
--- dbaas/physical/views.py
+++ dbaas/physical/views.py
@@ -20,6 +20,30 @@ def engines_by_env(self, environment_id):
     return HttpResponse(response_json, content_type="application/json")
 
 
+def topology_by_eng(self, engine_id):
+    engine = Engine.objects.get(id=engine_id)
+    replication_topologies = engine.replication_topologies.all()
+
+    replication_topologies_id = []
+    for replication_topology in replication_topologies:
+        replication_topologies_id.append(replication_topology.pk)
+
+    response_json = json.dumps({
+        "topology": replication_topologies_id
+    })
+    return HttpResponse(response_json, content_type="application/json")
+
+
+def offering_by_engine(self, engine_id, environment_id):
+    engine = Engine.objects.get(id=engine_id)
+    environment = Environment.objects.get(id=environment_id)
+    plans = engine.plans.filter(environments=environment, is_active=True
+                                ).values('stronger_offering__cpus',
+                                         'stronger_offering__memory_size_mb',
+                                         'is_ha')
+    return HttpResponse(plans, content_type="application/json")
+
+
 def plans_details(self):
     context = {
         "environments" : Environment.objects.all(),
